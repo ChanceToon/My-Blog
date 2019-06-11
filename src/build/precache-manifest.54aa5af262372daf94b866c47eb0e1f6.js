546fadb76cd7bb15cda47b41b7763a270213614e
Chance Toon <Toon@toons-mbp.home>
2019-06-10 23:34:36 -0500

    First commit

---
+++ src/build/precache-manifest.54aa5af262372daf94b866c47eb0e1f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5638f1a3fbff72d32d5ac0f8e7dff02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c876ef53b1cf3b032842",
+    "url": "/static/css/main.93920a6e.chunk.css"
+  },
+  {
+    "revision": "bb660b66d97ca872da68",
+    "url": "/static/js/2.ad55af6c.chunk.js"
+  },
+  {
+    "revision": "c876ef53b1cf3b032842",
+    "url": "/static/js/main.9c3168bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
